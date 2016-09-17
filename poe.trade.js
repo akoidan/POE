582a5713bcf57f0ab84a9c1e76dcda95702a71a1
Andrew <nightmare.quake@Mail.ru>
2016-09-18 02:42:29 +0300

    Update poe.trade.js

--- poe.trade.js
+++ poe.trade.js
@@ -1,13 +1,12 @@
-console.log('ok');
 var uniqueIgn = [];
 var outStr = "";
-var blockPesonlist = [];
+var blockPesonlist = ["SsenseChampion", "MajsanIBajsan", "Luciferr", "BlowUpEarth", "TimothySanchez", "BVarianCream", "Sprooce", "AssSense", "Drugbabe", "NiknonUltraShield", "DAMN_SHES_HOT", "CapBigode", "Streeze", "LibritanniaCS", "SnKCreepingDeath", "OBAFGKM", "AntiBvortex", "Krhuljcina", "EssenceBug", "Loirinhadobanheiro", "ФобияБоли", "Злая_Тетька", "Yagul", "AtlasTrickster", "Atlas_saltA", "Keledrain", "SlingshotEssence", "DerpiusMinimus", "kushivthejag", "MickGordon", "Aoobixofeio", "JasonKnox", "Quintileous", "Fapioh", "JesseWeNeedToCook"];
 function execute() {
 	var ign =document.querySelectorAll('[data-ign]');
 	[].filter.call(ign, function(el) {
 		 return !document.querySelector('#'+el.id +' .currency');
 	}).filter(function(el){
-		return $('#'+el.id +':contains("Merciless Difficulty")');
+		return  $('#'+el.id +':contains("Merciless Difficulty")').length > 0;
 	}).forEach(function(el) {
 		var name = el.getAttribute('data-ign');
 		if (uniqueIgn.indexOf(name) < 0 && blockPesonlist.indexOf(name) < 0) {
@@ -17,9 +16,10 @@ function execute() {
 	uniqueIgn.forEach(function(el){
 		outStr += 'o.Insert("@'+el+' Hi, I would like to buy your Twice Enchanted in Standard. My offer is 8chaos.")\n';
 	});
-	console.log(outStr);
+	
 }
-
+execute();
+console.log(outStr);
 /*
 printMessage() {
 	o := Object()

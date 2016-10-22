388ecc5b2de4c78b7c4c5977791ea27aa4921656
deathangel908 <123asd123>
2016-10-22 16:51:46 +0300

    daemon shield

--- shieldChecker.py
+++ shieldChecker.py
@@ -2,6 +2,7 @@ import json
 import datetime
 import requests
 import time
+import daemon
 
 conf = {'id' : -1}
 headers = {
@@ -9,13 +10,16 @@ headers = {
 }
 url = 'http://poe.trade/search/eatehagoyesiut/live'
 
-while True:
-	response = requests.post(url, conf, headers)
-	d = json.loads(response.content.decode('utf-8'))
-	conf['id'] = d['newid']
-	print("{} :: {}".format(datetime.datetime.now(), str(d)))
-	if d.get('data'):
-		with open("/tmp/file", "w") as text_file:
-			text_file.write(d.get('data'))
-		break
-	time.sleep(10)
+with daemon.DaemonContext():
+	text_file = open("/tmp/shield.log", "w")
+	while True:
+		response = requests.post(url, conf, headers)
+		d = json.loads(response.content.decode('utf-8'))
+		conf['id'] = d['newid']
+		text_file.write("{} :: {}\n".format(datetime.datetime.now(), str(d)))
+		text_file.flush()
+		if d.get('data'):
+			with open("/tmp/shield.txt", "w") as out_report:
+				out_report.write(d.get('data'))
+		time.sleep(10)
+	text_file.close()

50c33ee373a4182c144ac0201d17026c637c1338
Deathangel908 <deathangel908@gmail.com>
2016-11-02 16:42:36 +0200

    replaced whisper message to a native one

--- poe.trade.js
+++ poe.trade.js
@@ -1,19 +1,11 @@
+var LOCAL_STORAGE_PERMA_BLOCK = 'c22c2dc12123b';
+var LOCAL_STORAGE_TODAY_BLOCK = '2cc2c123122db';
+var offer = ' My offer is 20 chaos';
+function extraFilter(el) {
+    return true;
+    //return $('#' + el.id + ':contains("Merciless Difficulty")').length > 0;
+}
 var ls = (function() {
-    const LOCAL_STORAGE_PERMA_BLOCK = 'blockedPerma2';
-    const LOCAL_STORAGE_TODAY_BLOCK = 'blockedToday42233';
-    var escaper = (function() {
-		const escapeMap = {
-			'"': '""',
-			'#': '{#}'
-		};
-		var replaceHtmlRegex = new RegExp("[" + Object.keys(escapeMap).join("") + "]","g");
-		this.encode = function(html) {
-			return html.replace(replaceHtmlRegex, function(s) {
-				return escapeMap[s];
-			});
-		}
-		return this;
-	})();
     this.getLocalStorage = function(key) {
         var value = localStorage.getItem(key);
         return value ? JSON.parse(value) : [];
@@ -40,38 +32,64 @@ var ls = (function() {
     this.set
     return this;
 })();
+function loadFileSaver(onReady) {
+    if (typeof saveAs == 'undefined') {
+        var rawUrl = 'https://rawgit.com/eligrey/FileSaver.js/master/FileSaver.js';
+        // download and run the script
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        head.appendChild(script);
+        script.onload = onReady;
+		script.src = rawUrl;
+    } else {
+        onReady();
+    }
+}
+var escaper = (function() {
+    const escapeMap = {
+        '"': '""',
+        '#': '{#}'
+    };
+    var replaceHtmlRegex = new RegExp("[" + Object.keys(escapeMap).join("") + "]","g");
+    this.encode = function(html) {
+        return html.replace(replaceHtmlRegex, function(s) {
+            return escapeMap[s];
+        });
+    }
+    return this;
+})();
 (function() {
-    var offer = ' My offer is 20 chaos';
     var blockPesonlist = ls.getPermaBlock();
     var blockedToday = ls.getTodayBlock();
     var outStr = "";
+    var prices = {}
+    var getParent = function (el) {
+    	return el.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    }
     var uniqueIgn = {};
-    var ign = document.querySelectorAll('[data-ign]');
-    [].filter.call(ign, function(el) {
-        return !document.querySelector('#' + el.id + ' .currency');
-    }).filter(function(el) {
-    	return true;
-        //return $('#' + el.id + ':contains("Merciless Difficulty")').length > 0;
-    }).forEach(function(el) {
+    var ign = document.querySelectorAll('.whisper-btn');
+    [].filter.call(ign, function(btn) {
+        return !document.querySelector('#' + getParent(btn).id + ' .currency');
+    }).filter(extraFilter).forEach(function(btn) {
+    	var el = getParent(btn);
         var name = el.getAttribute('data-ign');
         if (blockPesonlist.indexOf(name) < 0 && blockedToday.indexOf(name) < 0) {
-        ls.blockToday(name);
-        uniqueIgn[name] = {
-            tabName: el.getAttribute('data-tab'),
-            top: el.getAttribute('data-y'),
-            left: el.getAttribute('data-x'),
-            name: el.getAttribute('data-name')
-        };
+            ls.blockToday(name);
+            uniqueIgn[name] = btn;
         }
     });
     for (var name in uniqueIgn) {
         if (!uniqueIgn.hasOwnProperty(name))
-            continue;var el = uniqueIgn[name];
-        var tabInfo = el.tabName ? ' (stash tab ""' + escaper.encode(el.tabName) + '""; position: left ' + el.left + ", top " + el.top + ')' : '';
-        outStr += 'o.Insert("@' + name + ' Hi, I would like to buy your ' + el.name + ' in Standard' + tabInfo + '.' + offer + '")\n';
+            continue;
+        var message = whisperMessage(uniqueIgn[name]);
+        outStr += message + '\n';
     }
     if (outStr) {
         console.log(outStr);
+        loadFileSaver(function() {
+            saveAs(new Blob([outStr], {type: "text/plain;charset=utf-8"}), 'buyItemsList.txt');
+        });
     } else {
         console.error("No new entries found");
     }

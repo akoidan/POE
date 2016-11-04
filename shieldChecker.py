6187667fc1df3dd2647b46b58d2910c67aeb6fc3
Deathangel908 <deathangel908@gmail.com>
2016-11-04 22:42:01 +0200

    removed demon

--- shieldChecker.py
+++ shieldChecker.py
@@ -154,15 +154,6 @@ class PoeTradeDigger(object):
 			self.notifier.log(content)
 			self.notifier.pushbullet(content, title)
 
-if not IS_WIN:
-	import daemon
-	with daemon.DaemonContext():
-		digger = PoeTradeDigger()
-		while True:
-			try:
-				digger.check_all()
-			except:
-				pass
-else:
-	digger = PoeTradeDigger()
-	digger.check_all()
+
+digger = PoeTradeDigger()
+digger.check_all()
